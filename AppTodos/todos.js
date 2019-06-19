e93d2fc67d602021443021866c8de0c6d0ef9061
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-19 08:53:57 -0300

    App Todos com localStorage

--- AppTodos/todos.js
+++ AppTodos/todos.js
@@ -2,11 +2,9 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Jantar'
-];
+
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || []
 
 // Renderizar Todos 
 function renderTodos() {
@@ -39,6 +37,7 @@ function addTodo() {
     todos.push(todoText)
     inputElement.value = ''
     renderTodos()
+    saveToStorage()
 }
 
 // btnAdicionar - Adicionar Todo
@@ -48,4 +47,10 @@ buttonElement.onclick = addTodo
 function deleteTodo(pos){
     todos.splice(pos, 1)
     renderTodos()
+    saveToStorage()
+}
+
+// Sarvar em local storage
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos))
 }
