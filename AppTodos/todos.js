d7521aa71344fc486a5f277959e425483d0ae1f7
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-19 08:37:29 -0300

    App Todos - Parte 1 completa

--- AppTodos/todos.js
+++ AppTodos/todos.js
@@ -7,3 +7,45 @@ var todos = [
     'Estudar Javascript',
     'Jantar'
 ];
+
+// Renderizar Todos 
+function renderTodos() {
+    listElement.innerHTML = ''
+
+    for(todo of todos){
+        var todoElement = document.createElement('li')
+        var todoText = document.createTextNode(todo)
+
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos +')')
+        var linkText = document.createTextNode('Excluir')
+
+        linkElement.appendChild(linkText)
+        todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
+        listElement.appendChild(todoElement)
+    }
+}
+
+renderTodos()
+
+// Adicionar Todo a lista
+function addTodo() {
+    var todoText = inputElement.value
+
+    todos.push(todoText)
+    inputElement.value = ''
+    renderTodos()
+}
+
+// btnAdicionar - Adicionar Todo
+buttonElement.onclick = addTodo
+
+// Deletar Todo
+function deleteTodo(pos){
+    todos.splice(pos, 1)
+    renderTodos()
+}
